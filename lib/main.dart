400307efd0b0b0cefc8e3c4adbeb1b18e194226e
Luis Classe <Luisclass0299@gmail.com>
2026-06-26 15:16:45 -0400

    init

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Portfolio Website',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Portfolio Home Page!'),
     );
   }
 }
